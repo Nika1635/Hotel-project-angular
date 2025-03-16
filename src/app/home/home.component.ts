d5d715cdd1c0ac201dfe92d670ecb359464109ac
nika robakidze <nika70252@gmail.com>
2025-03-16 21:13:33 +0400

    home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../api.service';
 import { Carddetails } from '../interfaces/carddetails';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
