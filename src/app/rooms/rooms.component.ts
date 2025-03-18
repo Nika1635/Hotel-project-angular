33d8909061a2993f2f4fcb6889e5d3a8d2d5dce0
nika robakidze <nika70252@gmail.com>
2025-03-18 14:23:05 +0400

    filter

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,28 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ApiService } from '../api.service';
 import { Carddetails } from '../interfaces/carddetails';
 
 @Component({
   selector: 'app-rooms',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './rooms.component.html',
   styleUrl: './rooms.component.css'
 })
-export class RoomsComponent implements OnInit{
-  constructor(private api: ApiService){}
+export class RoomsComponent implements OnInit {
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
     this.getRooms()
   }
 
+  public form: FormGroup = new FormGroup({
+    roomTypeId: new FormControl(""),
+    priceFrom: new FormControl(""),
+    priceTo: new FormControl(""),
+    maximumGuests: new FormControl(""),
+    checkIn: new FormControl(""),
+    checkOut: new FormControl("")
+  })
+
   rooms: Carddetails[] = []
 
-  getRooms(){
+  getRooms() {
     this.api.getAllRooms().subscribe((data) => {
       this.rooms = data
       console.log(data)
     })
   }
 
+  submit(){
+    console.log(this.form)
+  }
 }
